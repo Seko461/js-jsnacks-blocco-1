efba6a105130241a563e1d0392b9d203ec616360
Seko461 <sekopierre@gmail.com>
2024-02-26 11:25:37 +0100

    snack3

---
+++ jsnack3/readme.md
@@ -0,0 +1,5 @@
+#instructions
+
+il software deve chiedere 10 volte di inserire un numero
+poi stampare la somma
+ciclare in for e in while
---
+++ jsnack3/script.js
@@ -0,0 +1,22 @@
+let somma = 0;
+for (let i = 0; i < 10; i++) {
+    let number = prompt('inserisci un numero');
+    let realNumber = parseInt(number);
+    somma += realNumber;
+}
+
+console.log('la somma di tutti i numeri è ' + somma);
+
+
+let i = 0;
+
+while (i < 10) {
+    let number = prompt('inserisci un numero');
+    let realNumber = parseInt(number);
+    somma += realNumber;
+
+    i++
+}
+
+console.log('la somma di tutti i numeri è ' + somma);
+
