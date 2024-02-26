eea820b7c911f8220d76ddbddc8ed953c4769692
Seko461 <sekopierre@gmail.com>
2024-02-26 13:04:31 +0100

    add while

--- Jsnack8/script.js
+++ Jsnack8/script.js
@@ -2,10 +2,21 @@ let somma = 0;
 
 let n = prompt('inserisci un numero di 4 cifre');
 
-for (let i = 0; i < n.length; i++) {
+// for (let i = 0; i < n.length; i++) {
+//     let x = n[i];
+//     x = Number(x);
+//     somma += x
+// }
+
+
+let i = 0;
+while (i < n.length) {
     let x = n[i];
     x = Number(x);
     somma += x
+
+
+    i++
 }
 console.log(somma);
 
