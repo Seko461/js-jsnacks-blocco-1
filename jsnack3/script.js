a8e9517f9eb8acb878c9b85f21c15211a5a5f7d1
Seko461 <sekopierre@gmail.com>
2024-02-26 11:27:38 +0100

    while commented

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -8,15 +8,15 @@ for (let i = 0; i < 10; i++) {
 console.log('la somma di tutti i numeri è ' + somma);
 
 
-let i = 0;
+// let i = 0;
 
-while (i < 10) {
-    let number = prompt('inserisci un numero');
-    let realNumber = parseInt(number);
-    somma += realNumber;
+// while (i < 10) {
+//     let number = prompt('inserisci un numero');
+//     let realNumber = parseInt(number);
+//     somma += realNumber;
 
-    i++
-}
+//     i++
+// }
 
-console.log('la somma di tutti i numeri è ' + somma);
+// console.log('la somma di tutti i numeri è ' + somma);
 
